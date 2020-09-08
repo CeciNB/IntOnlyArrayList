2c2fb06eeaa8b54c7cd6c736b1324cfbe4c8be33
Cecilie <cecilie@brandt.net>
2020-09-08 16:06:52 +0200

    clean up

--- src/dk/kea/IntOnlyArrayList.java
+++ src/dk/kea/IntOnlyArrayList.java
@@ -1,7 +1,73 @@
 package dk.kea;
 
 public class IntOnlyArrayList {
+    private int[] intArray;
 
-    public IntOnlyArrayList() {
+    public IntOnlyArrayList(){}
+
+    public int get(int index){
+        return intArray[index];
+    }
+
+    public void add(int value){
+        int[] list;
+        if (intArray == null){
+            list = new int[]{value};
+        } else {
+            list = new int[intArray.length + 1];
+            for (int i = 0; i < intArray.length; i++){
+                list[i] = intArray[i];
+            }
+            list[intArray.length] = value;
+        }
+        intArray = list;
+    }
+
+    public void remove(int index){
+        if (intArray == null || intArray.length == 0){
+            System.out.println("no intArrayList");
+        } else if (index >= intArray.length) {
+            System.out.println("no index: " + index);
+        } else {
+            int[] list = new int[intArray.length-1];
+            int j = 0;
+            for (int i = 0; i < intArray.length; i++){
+                if (i == index){
+                    i++;
+                }
+                if (list.length != 0) {
+                    list[j] = intArray[i];
+                    j++;
+                }
+            }
+            intArray = list;
+        }
+    }
+
+    public int size(){
+        if (intArray == null){
+            return 0;
+        } else {
+            return intArray.length;
+        }
+    }
+
+    public void add(int index, int value){
+
+    }
+
+    @Override
+    public String toString(){
+        if (intArray == null || intArray.length == 0) {
+            return "no intArrayList";
+        } else {
+            String result = "IntArrayList = {";
+            for (int i = 0; i < intArray.length; i++) {
+                result = result + intArray[i] + ", ";
+            }
+            result = result.substring(0, result.length() - 2);
+            result = result + "}";
+            return result;
+        }
     }
 }
--- src/dk/kea/Main.java
+++ src/dk/kea/Main.java
@@ -1,7 +1,31 @@
 package dk.kea;
 
 public class Main {
+
     public static void main(String[] args) {
-        IntOnlyArrayList li = new IntOnlyArrayList();
+
+        //instantier
+        IntOnlyArrayList il = new IntOnlyArrayList();
+
+        //Tilføj nogle tal
+        il.add(1);
+        il.add(17);
+        il.add(42);
+
+        //udskriv
+        System.out.println(il);
+
+        System.out.println("size: " + il.size());
+
+        System.out.println("index 1: " + il.get(1));
+
+        //fjern et element
+        il.remove(1);
+
+        //udskriv
+        System.out.println(il);
+        System.out.println("size: " + il.size());
+
     }
+
 }
